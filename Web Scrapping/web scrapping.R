0a76e63fba6754e66a1845483a98219ade33f2a0
khpasalkar <khpasalkar16@gmail.com>
2018-11-01 01:21:41 +0530

    add all files

---
+++ Web Scrapping/web scrapping.R
@@ -0,0 +1,25 @@
+library("rvest")
+library("stringr")
+library("tidyr")
+url <- 'http://espn.go.com/nfl/superbowl/history/winners'
+webpage <- read_html(url)
+sb_table <- html_nodes(webpage, 'table') #extracting contents of table tab from html web page
+sb <- html_table(sb_table)[[1]] #creating a data frame of the contents that were extracted above
+head(sb)
+sb <- sb[-(1:2),] #removing first 2 rows 
+names(sb) <- c("number", "date", "site", "result") #giving labels to each column
+head(sb)
+sb$number <- 1:50 #converting roman numbers to actual numbers
+head(sb)
+sb$date <- as.Date(sb$date, "%B.%d, %Y") #converting dates to conventional form
+head(sb)
+sb <- separate(sb, result, c("winner", "loser"), sep = ",", remove = TRUE) #separating results column as winner and loser
+head(sb)
+pattern <- "\\d+$" #for space between 2 elements, $ begins extracting from end
+sb$winnerscore <- as.numeric(str_extract(sb$winner,pattern)) #extracting winner score from winner column
+sb$loserscore <- as.numeric(str_extract(sb$loser, pattern)) #extracting loser score from loser column
+sb$winner <- gsub(pattern, "", sb$winner) #replaces all occurences with the alternate given, so here all space patterns are replaced by blank thus removing winner score from winner
+sb$loser <- gsub(pattern, "", sb$loser) #same as above for loser column
+head(sb)
+write.csv(sb,"Superbowl",row.names = FALSE)
+#gsub and sub are 2 ways to remove or subsetting of an element 
